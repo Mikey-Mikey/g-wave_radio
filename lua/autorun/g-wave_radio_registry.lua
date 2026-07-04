e398a04f3c96210577789f7f0e171175333ea762
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-07-04 10:22:40 +0300

    Use util.IsValidModel to prevent differences in selection on the server and client

--- lua/autorun/g-wave_radio_registry.lua
+++ lua/autorun/g-wave_radio_registry.lua
@@ -3,7 +3,11 @@ GWAVE.RegisterRadio( "g-wave_radio_bigtv", "models/props/cs_militia/tv_console.m
 GWAVE.RegisterRadio( "g-wave_radio_small", "models/props/cs_office/radio.mdl", "Small Radio" )
 GWAVE.RegisterRadio( "g-wave_radio_smalltv", "models/props/de_inferno/tv_monitor01.mdl", "Small TV" )
 
-if IsMounted( "csgo" ) then
+-- CS:GO Models
+if util.IsValidModel( "models/props/de_inferno/hr_i/inferno_vintage_radio/inferno_vintage_radio.mdl" ) then
 	GWAVE.RegisterRadio( "g-wave_radio_vintage", "models/props/de_inferno/hr_i/inferno_vintage_radio/inferno_vintage_radio.mdl", "Vintage Radio" )
+end
+
+if util.IsValidModel( "models/props/cs_italy/radio_wooden.mdl" ) then
 	GWAVE.RegisterRadio( "g-wave_radio_wooden", "models/props/cs_italy/radio_wooden.mdl", "Wooden Radio" )
 end
